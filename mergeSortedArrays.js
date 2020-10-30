455f0a048c6c2b0b3590c8f79c86f0a6c1e69392
vivianP <vivianctvpham@gmail.com>
2020-10-29 17:28:14 -0700

    Added in mergeSortedArray solution

---
+++ mergeSortedArrays.js
@@ -0,0 +1,39 @@
+function mergeSortedArrays(arr1, arr2) {
+    const mergedArray = [];
+    let array1Item = arr1[0];
+    let array2Item = arr2[0];
+    let i = 1;
+    let j = 1;
+
+    //Check input 
+    if (arr1.length == 0) return arr2;
+    if (arr2.length == 0) return arr1;
+
+    while (array1Item || array2Item) {
+        if (!array2Item || array1Item < array2Item) {
+            mergedArray.push(array1Item);
+            array1Item = arr1[i];
+            i++;
+        } else {
+            mergedArray.push(array2Item);
+            array2Item = arr2[j];
+            j++;
+        }
+    }
+
+
+    return mergedArray;
+}
+
+
+
+
+
+
+
+
+function main() {
+    console.log(mergeSortedArrays([0,3,4,31], [4,6,30]));
+}
+
+main();
